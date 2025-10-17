9c4abfc335ffd8645978b64b47245c0b7bec93d4
Rohan Belsare <rohanbelsare113@gmail.com>
2025-10-17 13:52:23 +0530

    new changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -35,6 +37,10 @@ const nextConfig = {
       },
     ];
   },
-}
+};
 
-export default nextConfig
+export default withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
